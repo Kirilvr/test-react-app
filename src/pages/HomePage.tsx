3a7a364004e1222fa404862c52e209aff11244fd
Kirilvr <83722123+Kirilvr@users.noreply.github.com>
2022-08-05 12:36:53 +0300

    add getUserRepos endpoint

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import { useSearchUsersQuery } from '../store/github/github.api';
+import {
+  useLazyGetUserReposQuery,
+  useSearchUsersQuery,
+} from '../store/github/github.api';
 import { useDebounce } from '../hooks/debounce';
 
 interface IHomePage {}
@@ -10,11 +13,19 @@ export const HomePage = ({}: IHomePage) => {
   const debounced = useDebounce(search);
   const { isLoading, isError, data } = useSearchUsersQuery(debounced, {
     skip: debounced.length < 3,
+    refetchOnFocus: true,
   });
+
+  const [fetchRepos, { isLoading: areReposLoading, data: repos }] =
+    useLazyGetUserReposQuery();
 
   useEffect(() => {
     setDropdown(debounced.length > 3 && data?.length! > 0);
   }, [debounced, data]);
+
+  const clickHandler = (username: string) => {
+    fetchRepos(username);
+  };
 
   return (
     <div className="flex justify-center pt-10 mx-auto h-screen w-screen">
@@ -24,7 +35,7 @@ export const HomePage = ({}: IHomePage) => {
       <div className="relative w-[560px]">
         <input
           type="text"
-          className="border py-2 px-4 w-screen h-[42px] mb-2"
+          className="border py-2 px-4 w-[560px] h-[42px] mb-2"
           placeholder="Search for Github username..."
           value={search}
           onChange={(e) => setSearch(e.target.value)}
@@ -36,12 +47,21 @@ export const HomePage = ({}: IHomePage) => {
               <li
                 key={user.id}
                 className="py-2 px-4 hover:bg-gray-500 hover:text-white transition-colors cursor-pointer"
+                onClick={() => clickHandler(user.login)}
               >
                 {user.login}
               </li>
             ))}
           </ul>
         )}
+        <div className="container">
+          {areReposLoading && (
+            <p className="text-center">Repos are loading...</p>
+          )}
+          {repos?.map((repo) => (
+            <p key={repo.id}>{repo.url}</p>
+          ))}
+        </div>
       </div>
     </div>
   );
